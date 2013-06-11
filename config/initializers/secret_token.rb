70f792fac7401c8ecb478c8d8c2c88d18d196e46
Luis Mender <luismender@luis.(none)>
2013-06-10 23:17:37 -0300

    [Initial Commit]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogLau::Application.config.secret_token = '79b368c2b59953366751c68f22cd3424c3fe9e9e347c24fa22e93c5b31bc604e07e09c71fb3a66ff464f2e6ea7035557a694a8a064d6b9126440ab1120175ff7'
